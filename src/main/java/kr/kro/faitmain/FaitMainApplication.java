7ad9e59385d754b2374ec435353b1dcb82253bc7
wumeenna <minu960604@gmail.com>
2022-05-23 18:58:00 +0900

    first commit

---
+++ src/main/java/kr/kro/faitmain/FaitMainApplication.java
@@ -0,0 +1,13 @@
+package kr.kro.faitmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaitMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaitMainApplication.class, args);
+	}
+
+}
